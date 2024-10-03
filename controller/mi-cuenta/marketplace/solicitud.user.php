ff149e5d5f9d82e36e151edac9438ab0791452e7
ussiel <ussiel@gmail.com>
2024-10-03 10:41:52 -0600

    actualizacion de composer

---
+++ controller/mi-cuenta/marketplace/solicitud.user.php
@@ -0,0 +1,5 @@
+<?php
+use Catalog\Form\UserMarketplaceForm;
+
+$MyFiltrosForm = new UserMarketplaceForm('frmusermarketplace');
+$MyFiltrosForm->setMobile($Mobile_detect->isMobile());
---
+++ src/Form/UserMarketplaceForm.php
@@ -0,0 +1,97 @@
+<?php
+namespace Catalog\Form;
+
+class UserMarketplaceForm extends \Franky\Form\Form
+{
+    public function __construct($name)
+    {
+
+        parent::__construct();
+
+       $this->setAtributos(array(
+            'name' => $name,
+            'action' =>  "/mi-cuenta/marketplace/submit.user.php",
+            'method' => 'post',
+            'enctype' => "multipart/form-data"
+        ));
+
+
+
+        $this->add(array(
+                'name' => 'rfc',
+                'label' => _catalog('RFC'),
+                'type'  => 'text',
+                'required'  => true,
+                'atributos' => array(
+                    'class'       => 'required',
+                    'maxlength' => 13
+                ),
+                'label_atributos' => array(
+                    'class'       => 'desc_form_obligatorio'
+                )
+            )
+        );
+
+        $this->add(array(
+            'name' => 'store',
+            'label' => _catalog('Tienda'),
+            'type'  => 'select',
+            'required'  => true,
+        'required'  => true,
+            'atributos' => array(
+                'class'       => 'required'
+            ),
+            'options' => array(
+
+            ),
+            'label_atributos' => array(
+                'class'       => 'desc_form_obligatorio'
+            )
+            )
+        ); 
+
+
+        $this->add(array(
+            'name' => 'ine_anverso',
+            'label' => _catalog('INE anverso'),
+            'type'  => 'file',
+            'atributos' => array(
+                'id' => "image_inea"
+                )
+            )
+        );
+        $this->add(array(
+            'name' => 'ine_reverso',
+            'label' => _catalog('INE reverso'),
+            'type'  => 'file',
+            'atributos' => array(
+                'id' => "image_iner"
+                )
+            )
+        );
+        $this->add(array(
+            'name' => 'comprobante',
+            'label' => _catalog('Comprobante de domicilio'),
+            'type'  => 'file',
+            'atributos' => array(
+                'id' => "image_comprobante"
+                )
+            )
+        );
+
+
+         $this->add(array(
+                'name' => 'guardar',
+                'type'  => 'submit',
+                'atributos' => array(
+                    'class'       => 'btn btn-primary btn-big float_right',
+                    'value' => _catalog("Guardar")
+                 )
+
+            )
+        );
+
+    }
+
+}
+?>
